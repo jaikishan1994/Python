c88c91184a279bd55f7ded47b38c837f089223e8
Jai Kishan Kumar <37844679+jaikishan1994@users.noreply.github.com>
2018-09-03 11:24:37 +0530

    bug fix_UI_
    fuctionality issue is fixed no2

--- 2.py
+++ 2.py
@@ -8,5 +8,5 @@ Created on Fri Aug 10 03:30:13 2018
 total = input("What's the total cost?");
 
 On_Discount = int(total) * .9;
-print("Discount = ",int(total)*.1);
-print("Total = ",int(total)*.9);
+print("Discount = ",int(total)*.1)
+print("Hi")
